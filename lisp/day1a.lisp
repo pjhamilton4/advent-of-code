4de7b45b22aab7631583af73207cba3ca2570fd7
Philip Hamilton <pjhamilton4@gmail.com>
2019-12-02 01:14:49 -0500

    Updated with test case showing WRONG math. To be fixed later

--- lisp/day1a.lisp
+++ lisp/day1a.lisp
@@ -1,23 +1,24 @@
 (defun calculate-value(input sum)
-;  (format t "Input ~D " input)
-;  (format t "Sum ~D | " sum)
+  (format t "Input ~D " input)
+  (format t "Sum ~D | " sum)
   (let ((total 0))
   (cond ((<= input 0) (+ sum 2))
-	((= input 1) sum)
-	((> input 1) (progn 
+	((= input 1) total)
+	((>= input 2) (progn 
 		       (setf total (+ sum (- (floor (/ input 3)) 2))) 
 		       (calculate-value (- (floor (/ input 3)) 2) total)
 		       )))
 ))
 
-(write (let ((in (open "../day-1a-input.txt" :if-does-not-exist nil)))
-  (when in
-    (let ((sum 0))
-    (loop for line = (read-line in nil nil)
-      while line
-      do
-      (setf sum (+ sum (calculate-value (parse-integer line) 0)))
-    )sum))))
+;(write (let ((in (open "../day-1a-input.txt" :if-does-not-exist nil)))
+;  (when in
+;    (let ((sum 0))
+;    (loop for line = (read-line in nil nil)
+;      while line
+;      do
+;      (setf sum (+ sum (calculate-value (parse-integer line) 0)))
+;    )sum))))
 
-;(write (calculate-value 87816 0))
+
+(write (calculate-value 1969 0))
 (terpri)
