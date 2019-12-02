160cef1e1ffdbe9cf0d36f9e3b575e5b43451dc2
Philip Hamilton <pjhamilton4@gmail.com>
2019-12-02 10:10:52 -0500

    Fixed day 1 part 2 issue, finished and working!

--- lisp/day1a.lisp
+++ lisp/day1a.lisp
@@ -1,24 +1,27 @@
 (defun calculate-value(input sum)
-  (format t "Input ~D " input)
-  (format t "Sum ~D | " sum)
+;  (format t "Input ~D " input)
+;  (format t "Sum ~D | " sum)
   (let ((total 0))
-  (cond ((<= input 0) (+ sum 2))
-	((= input 1) total)
-	((>= input 2) (progn 
+  (cond ((<= input 5) sum)
+	((> input 5) (progn 
 		       (setf total (+ sum (- (floor (/ input 3)) 2))) 
 		       (calculate-value (- (floor (/ input 3)) 2) total)
 		       )))
 ))
 
-;(write (let ((in (open "../day-1a-input.txt" :if-does-not-exist nil)))
-;  (when in
-;    (let ((sum 0))
-;    (loop for line = (read-line in nil nil)
-;      while line
-;      do
-;      (setf sum (+ sum (calculate-value (parse-integer line) 0)))
-;    )sum))))
+(write (let ((in (open "../day-1a-input.txt" :if-does-not-exist nil)))
+  (when in
+    (let ((sum 0))
+    (loop for line = (read-line in nil nil)
+      while line
+      do
+      (setf sum (+ sum (calculate-value (parse-integer line) 0)))
+    )sum))))
 
 
-(write (calculate-value 1969 0))
+;(write (calculate-value 53247 0))
+;(terpri)
+;(write (calculate-value 140268 0))
+;(terpri)
+;(write (calculate-value 139961 0))
 (terpri)
