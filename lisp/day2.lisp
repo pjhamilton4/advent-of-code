43655ce62c71631177305bbed602b7125aea5d5e
Philip Hamilton <pjhamilton4@gmail.com>
2019-12-05 03:22:52 -0500

    Day 2 part 1 is working!

--- lisp/day2.lisp
+++ lisp/day2.lisp
@@ -1,23 +1,30 @@
-
 (load "~/quicklisp/setup.lisp")
 (ql:quickload :cl-ppcre)
 
 (defun read-program(input)
-  (setq input-number-array (mapcar #'parse-integer (cl-ppcre:split "(,)" input)))
+  (setf input-number-array (make-array (length input) :initial-contents input))
 
-  ; Get 4 entries from input .
-  (setq op (nth 0 input-number-array))
-  (setq first-num (nth 1 input-number-array))
-  (setq second-num (nth 2 input-number-array))
-  (setq write-position (nth 3 input-number-array))
-  (if (= op 1) (write (+ first-num second-num)) (write (* first-num second-num)))
+  (setq i 0)
+  (loop while (< i (length input))
+	do
+	(let ((op (aref input-number-array i)))
+	      (cond ((eql op 1) (setf (aref input-number-array (aref input-number-array (+ i 3))) 
+				      (+ (aref input-number-array (aref input-number-array (+ i 1))) 
+					 (aref input-number-array (aref input-number-array (+ i 2))))))
+		    ((eql op 2) (setf (aref input-number-array (aref input-number-array (+ i 3))) 
+				      (* (aref input-number-array (aref input-number-array (+ i 1)))
+					 (aref input-number-array (aref input-number-array (+ i 2))))))
+		    ((eql op 99) (return))))
+	(setq i (+ i 4)))
+
+;  (print input-number-array)
+  (print (aref input-number-array 0))
   )
 
 (write (let ((in (open "../day-2-input.txt" :if-does-not-exist nil)))
 	 (when in
 	   (loop for line = (read-line in nil)
-		 while line do (if (not nil)(read-program line))
+		 while line do (if (not nil)(read-program (mapcar #'parse-integer (cl-ppcre:split "(,)" line))))
 		 ))))
 
-       (terpri)
-
+(terpri)
